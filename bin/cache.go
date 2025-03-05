8771a857a02fc2946cc7d37909b3e24d929aa25f
Александр <red.hoodie25@gmail.com>
2025-03-05 17:11:58 +0300

    update cache

--- bin/cache.go
+++ bin/cache.go
@@ -6,10 +6,19 @@ import (
 )
 
 type Cache struct {
-	LastPostID int `json:"last_post_id"`
+	LastPostID int         `json:"last_post_id"`
+	Posts      map[int]int `json:"posts"`
 }
 
 func LoadCache(path string) (*Cache, error) {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+
+		return &Cache{
+			LastPostID: 0,
+			Posts:      make(map[int]int),
+		}, nil
+	}
+
 	file, err := os.Open(path)
 	if err != nil {
 		return nil, err
@@ -22,17 +31,22 @@ func LoadCache(path string) (*Cache, error) {
 		return nil, err
 	}
 
+	if cache.Posts == nil {
+		cache.Posts = make(map[int]int)
+	}
+
 	return &cache, nil
 }
 
 func SaveCache(path string, cache *Cache) error {
-	file, err := os.Open(path)
+	file, err := os.Create(path)
 	if err != nil {
 		return err
 	}
 	defer file.Close()
 
 	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "  ")
 	err = encoder.Encode(cache)
 	if err != nil {
 		return err
