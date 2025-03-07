96455ce7f00b71c5e1b825e70a8563ed29ace054
Александр <red.hoodie25@gmail.com>
2025-03-07 21:22:31 +0300

    goroutines

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"sync"
 	"time"
 
 	"gorepostbot/bin"
@@ -34,6 +35,22 @@ func main() {
 	}
 	log.Printf("Загружен кэш: %+v", cache)
 
+	cacheUpdates := make(chan struct{}, 10)
+
+	go func() {
+		for range cacheUpdates {
+			err := bin.SaveCache(cfg.CacheFile, cache)
+			if err != nil {
+				log.Printf("Не удалось сохранить кэш: %v", err)
+			} else {
+				log.Printf("Кэш успешно сохранен")
+			}
+
+			// Ограничиваем частоту сохранений
+			time.Sleep(5 * time.Second)
+		}
+	}()
+
 	for {
 		posts, err := vkClient.GetWallPosts(cfg.TargetUser, 10)
 		if err != nil {
@@ -42,55 +59,91 @@ func main() {
 			continue
 		}
 
+		if len(posts) == 0 {
+			log.Println("Новых постов не обнаружено")
+			time.Sleep(time.Duration(cfg.PollInterval) * time.Second)
+			continue
+		}
+
+		var cacheMutex sync.Mutex
+		var wg sync.WaitGroup
+
 		for _, post := range posts {
 			if post.ID <= cache.LastPostID {
 				continue
 			}
 
-			tgMessageIDs, err := tgClient.SendMessage(post.Text)
-			if err != nil {
-				log.Printf("Не удалось отправить сообщение: %v", err)
-				continue
-			}
+			wg.Add(1)
+			go func(p lib.VKPost) {
+				defer wg.Done()
 
-			for _, attachment := range post.Attachments {
-				switch attachment.Type {
-				case "photo":
-					if attachment.Photo != nil {
-						lastSize := attachment.Photo.Sizes[len(attachment.Photo.Sizes)-1]
-						err := tgClient.SendPhoto(lastSize.URL)
-						if err != nil {
-							log.Printf("Не удалось отправить фото: %v", err)
-						}
-					}
-				case "video":
-					log.Printf("Видео ещё не поддерживается: %+v", attachment)
-				default:
-					log.Printf("Неподдерживаемый вид: %s", attachment.Type)
+				log.Printf("Обработка поста ID %d", p.ID)
+				tgMessageIDs, err := tgClient.SendMessage(p.Text)
+				if err != nil {
+					log.Printf("Не удалось отправить сообщение: %v", err)
+					return
 				}
-			}
 
-			for _, tgMessageID := range tgMessageIDs {
-				cache.AddPost(bin.Post{
-					VKRecordID:   post.ID,
-					TGMessageID:  tgMessageID,
-					LastModified: post.Date,
-				})
-			}
+				if len(p.Attachments) > 0 {
+					var attachWg sync.WaitGroup
 
-			if cache.LastPostID == 0 {
-				cache.LastPostID = posts[0].ID
-			}
+					for _, attachment := range p.Attachments {
+						attachWg.Add(1)
+						go func(att lib.VKAttachment) {
+							defer attachWg.Done()
+
+							switch att.Type {
+							case "photo":
+								if att.Photo != nil {
+									lastSize := att.Photo.Sizes[len(att.Photo.Sizes)-1]
+									err := tgClient.SendPhoto(lastSize.URL)
+									if err != nil {
+										log.Printf("Не удалось отправить фото: %v", err)
+									} else {
+										log.Printf("Отправлено фото для поста %d", p.ID)
+									}
+								}
+							case "video":
+								log.Printf("Видео ещё не поддерживается: %+v", att)
+							default:
+								log.Printf("Неподдерживаемый вид: %s", att.Type)
+							}
+						}(attachment)
+					}
+
+					attachWg.Wait()
+				}
+
+				cacheMutex.Lock()
+				defer cacheMutex.Unlock()
+
+				for _, tgMessageID := range tgMessageIDs {
+					cache.AddPost(bin.Post{
+						VKRecordID:   p.ID,
+						TGMessageID:  tgMessageID,
+						LastModified: p.Date,
+					})
+				}
+
+				if p.ID > cache.LastPostID {
+					cache.LastPostID = p.ID
+					log.Printf("Обновлен LastPostID: %d", cache.LastPostID)
+				}
+
+				select {
+				case cacheUpdates <- struct{}{}:
+				default:
+				}
+			}(post)
 		}
 
-		err = bin.SaveCache(cfg.CacheFile, cache)
-		if err != nil {
-			log.Printf("Не удалось сохранить кэш %v", err)
+		wg.Wait()
+
+		select {
+		case cacheUpdates <- struct{}{}:
+		default:
 		}
 
 		time.Sleep(time.Duration(cfg.PollInterval) * time.Second)
-		if err != nil {
-			fmt.Println("Ошибка:", err)
-		}
 	}
 }
